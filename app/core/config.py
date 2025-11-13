7da01445604175ecfa9d0b0932212a7537a8fd40
ayyyy/ <ack159ack@gmail.com>
2025-11-13 19:25:17 +0800

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -20,7 +20,7 @@ class Settings(BaseSettings):
 
     # --- 模型列表 (从抓包数据中分析得出) ---
     SUPPORTED_MODELS: List[str] = [
-        "ERNIE5.0Preview1111", # 对应 EB50
+        "ernie-5.0-Preview-1111", # 对应 EB50
         "ernie-4.5-turbo", # 对应 EB45T
         "ernie-x1",        # 对应 X1_1
     ]
@@ -37,3 +37,4 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
